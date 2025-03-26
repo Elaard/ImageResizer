b3370063e8dad82123fb8c1ccc9d5eecab303600
Elard <bartek.dv.89@gmail.com>
2025-03-26 10:19:29 +0100

    fix docker calls

--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -17,10 +17,19 @@ builder.Services.AddSingleton<IAmazonDynamoDB>(sp =>
 {
     var credentials = new BasicAWSCredentials("faceAccessKey", "fakeSecretPassword");
 
+    // Determine if running in Docker
+    var isInDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+    
+    // When running in Docker, use host.docker.internal to access the host machine
+    // When running locally, use localhost
+    var dynamoDbHost = isInDocker ? "host.docker.internal" : "localhost";
+    
+    Console.WriteLine($"Connecting to DynamoDB at {dynamoDbHost}:8000");
+    
     var clientConfig = new AmazonDynamoDBConfig
     {
         RegionEndpoint = Amazon.RegionEndpoint.USEast1,
-        ServiceURL = "http://localhost:8000"
+        ServiceURL = $"http://{dynamoDbHost}:8000"
     };
 
     return new AmazonDynamoDBClient(credentials, clientConfig);
